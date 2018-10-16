8e994a3fa587afd40757da762d881f88476f8d07
Erika Wolfe <erikaw@allenai.org>
2018-10-16 09:44:07 -0700

    Add status as a group search field

--- api/searchfield/group_tasks.go
+++ api/searchfield/group_tasks.go
@@ -5,6 +5,7 @@ type GroupTask string
 const (
 	GroupTaskID         GroupTask = "task_id"
 	GroupTaskName       GroupTask = "task_name"
+	GroupTaskStatus     GroupTask = "task_status"
 	GroupExperimentID   GroupTask = "experiment_id"
 	GroupExperimentName GroupTask = "experiment_name"
 )
