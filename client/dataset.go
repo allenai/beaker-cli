b4938e41eade05e969288e21b1c2dda775d69aba
Lane Aasen <lanea@allenai.org>
2018-12-06 18:03:09 -0800

    Add DatasetHandle.Files to replace Manifest

--- client/dataset.go
+++ client/dataset.go
@@ -6,9 +6,12 @@ import (
 	"net/url"
 	"path"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/allenai/beaker/api"
 	fileheap "github.com/allenai/fileheap-client/client"
+	"github.com/pkg/errors"
 )
 
 // DatasetHandle provides operations on a dataset.
@@ -106,6 +109,7 @@ func (h *DatasetHandle) Get(ctx context.Context) (*api.Dataset, error) {
 }
 
 // Manifest retrieves a manifest for a dataset's contents.
+// Deprecated. Use Files() instead.
 func (h *DatasetHandle) Manifest(ctx context.Context) (*api.DatasetManifest, error) {
 	path := path.Join("/api/v3/datasets", h.id, "manifest")
 	resp, err := h.client.sendRequest(ctx, http.MethodGet, path, nil, nil)
@@ -119,6 +123,99 @@ func (h *DatasetHandle) Manifest(ctx context.Context) (*api.DatasetManifest, err
 		return nil, err
 	}
 	return &body, nil
+}
+
+// FileIterator is an iterator over files within a dataset.
+type FileIterator interface {
+	Next() (*FileHandle, *FileInfo, error)
+}
+
+// Files returns an iterator over all files in the dataset under the given path.
+func (h *DatasetHandle) Files(ctx context.Context, path string) (FileIterator, error) {
+	if h.pkg != nil {
+		return &packageFileIterator{
+			dataset:    h,
+			fhIterator: h.pkg.Files(ctx, path),
+		}, nil
+	}
+
+	manifest, err := h.Manifest(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &manifestFileIterator{
+		dataset: h,
+		files:   manifest.Files,
+	}, nil
+}
+
+// FileInfo describes a single file within a dataset.
+type FileInfo struct {
+	// Path of the file relative to its dataset root.
+	Path string `json:"path"`
+
+	// Size of the file in bytes.
+	Size int64 `json:"size"`
+
+	// Time at which the file was last updated.
+	Updated time.Time `json:"updated"`
+}
+
+// ErrDone indicates an iterator is expended.
+var ErrDone = errors.New("no more items in iterator")
+
+// packageFileIterator is an iterator over files within a FileHeap package.
+type packageFileIterator struct {
+	dataset    *DatasetHandle
+	fhIterator *fileheap.FileIterator
+}
+
+func (i *packageFileIterator) Next() (*FileHandle, *FileInfo, error) {
+	ref, info, err := i.fhIterator.Next()
+	if err == fileheap.ErrDone {
+		return nil, nil, ErrDone
+	}
+	if err != nil {
+		return nil, nil, err
+	}
+	return &FileHandle{
+			dataset: i.dataset,
+			file:    info.Path,
+			fileRef: ref,
+		}, &FileInfo{
+			Path:    info.Path,
+			Size:    info.Size,
+			Updated: info.Updated,
+		}, nil
+}
+
+// manifestFileIterator is an iterator over files in a dataset manifest.
+type manifestFileIterator struct {
+	dataset *DatasetHandle
+	files   []api.DatasetFile
+	prefix  string
+}
+
+func (i *manifestFileIterator) Next() (*FileHandle, *FileInfo, error) {
+	for len(i.files) > 0 && !strings.HasPrefix(i.files[0].File, i.prefix) {
+		i.files = i.files[1:]
+	}
+	if len(i.files) == 0 {
+		return nil, nil, ErrDone
+	}
+
+	file := i.files[0]
+	i.files = i.files[1:]
+
+	return &FileHandle{
+			dataset: i.dataset,
+			file:    file.File,
+		}, &FileInfo{
+			Path:    file.File,
+			Size:    int64(file.Size),
+			Updated: file.TimeLastModified,
+		}, nil
 }
 
 // SetName sets a dataset's name.
