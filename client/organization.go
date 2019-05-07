96b022456013318bebf2c1750e6df70d31c7b2b2
Colin Arenz <11339146+ckarenz@users.noreply.github.com>
2019-05-07 16:32:21 -0700

    Further clarify

--- client/organization.go
+++ client/organization.go
@@ -8,19 +8,13 @@ import (
 	"github.com/pkg/errors"
 )
 
-// VerifyOrgExists gets a handle for an org by name or ID. The returned handle is
-// guaranteed throughout its lifetime to refer to the same object, even if that
-// object is later renamed.
+// VerifyOrgExists validates existence of an organization.
 func (c *Client) VerifyOrgExists(ctx context.Context, org string) error {
 	resp, err := c.sendRequest(ctx, http.MethodGet, path.Join("/api/v3/orgs", org), nil, nil)
 	defer safeClose(resp.Body)
 	if err != nil {
-		return errors.WithMessage(err, "could not resolve org reference "+org)
+		return errors.WithMessage(err, "could not resolve organization "+org)
 	}
 
-	var body struct {
-		ID string `json:"id"`
-	}
-
-	return parseResponse(resp, &body)
+	return errors.WithMessage(errorFromResponse(resp), org)
 }
