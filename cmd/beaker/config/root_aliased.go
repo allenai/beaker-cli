c482aecfba959378db7e6dcea1fccbbf741edab5
Michael Langan <michaell@allenai.org>
2019-09-05 11:10:46 -0700

    Make 'configure' behave identically to old 'configure'

--- cmd/beaker/config/root_aliased.go
+++ cmd/beaker/config/root_aliased.go
@@ -4,28 +4,21 @@ import (
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/allenai/beaker/cmd/beaker/options"
-	"github.com/allenai/beaker/config"
+	beakerConfig "github.com/allenai/beaker/config"
 )
 
-// NewConfigureCmd duplicates the "config" command under "configure" for back-compat
+// NewConfigureCmd replicates the old "configure" for back-compat
 func NewConfigureCmd(
 	parent *kingpin.Application,
 	parentOpts *options.AppOptions,
-	config *config.Config,
+	config *beakerConfig.Config,
 ) {
 	o := &configOptions{AppOptions: parentOpts}
 	cmd := parent.Command("configure", "Manage Beaker configuration settings")
-
-	// Add automatic help generation for the command group.
-	var helpSubcommands []string
-	cmd.Command("help", "Show help.").Hidden().Default().PreAction(func(c *kingpin.ParseContext) error {
-		fullCommand := append([]string{cmd.Model().Name}, helpSubcommands...)
-		parent.Usage(fullCommand)
-		return nil
-	}).Arg("command", "Show help on command.").StringsVar(&helpSubcommands)
+	cmd.Command("interactive", "Interactive configuration").Default().Action(func(c *kingpin.ParseContext) error {
+		return beakerConfig.InteractiveConfiguration()
+	})
 
 	// Attach subcommands.
-	newInteractiveCmd(cmd, o, config)
-	newSetCmd(cmd, o, config)
 	newTestCmd(cmd, o, config)
 }
