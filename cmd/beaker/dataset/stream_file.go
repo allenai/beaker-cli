739aaebdf0fe18c7049021c2fe27d9ee7ff12833
Vivek Lakshmanan <vivekl@allenai.org>
2019-12-19 12:09:24 -0800

    Fix test

--- cmd/beaker/dataset/stream_file.go
+++ cmd/beaker/dataset/stream_file.go
@@ -48,19 +48,17 @@ func (o *streamFileOptions) run(beaker *client.Client) error {
 		return err
 	}
 
-	var fileRef *client.FileHandle
 	if o.file == "" {
 		return errors.Errorf("filename required for dataset %s", dataset.ID())
+	}
+	files, err := dataset.Files(ctx, "")
+	if err != nil {
+		return err
+	}
 
-		files, err := dataset.Files(ctx, "")
-		if err != nil {
-			return err
-		}
-		if fileRef, _, err = files.Next(); err != nil {
-			return err
-		}
-	} else {
-		fileRef = dataset.FileRef(o.file)
+	var fileRef *client.FileHandle
+	if fileRef, _, err = files.Next(); err != nil {
+		return err
 	}
 
 	var r io.ReadCloser
