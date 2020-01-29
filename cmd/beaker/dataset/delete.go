3efdca23921d0adc72d761ca46035419a61f5784
Lane Aasen <lanea@allenai.org>
2020-01-29 14:22:56 -0800

    Add dataset delete command

---
+++ cmd/beaker/dataset/delete.go
@@ -0,0 +1,44 @@
+package dataset
+
+import (
+	"context"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+
+	beaker "github.com/beaker/client/client"
+
+	"github.com/allenai/beaker/config"
+)
+
+type deleteOptions struct {
+	dataset string
+}
+
+func newDeleteCmd(
+	parent *kingpin.CmdClause,
+	parentOpts *datasetOptions,
+	config *config.Config,
+) {
+	o := &deleteOptions{}
+	cmd := parent.Command("delete", "Permanently delete a dataset")
+	cmd.Action(func(c *kingpin.ParseContext) error {
+		beaker, err := beaker.NewClient(parentOpts.addr, config.UserToken)
+		if err != nil {
+			return err
+		}
+		return o.run(beaker)
+	})
+
+	cmd.Arg("dataset", "Dataset name or ID").Required().StringVar(&o.dataset)
+}
+
+func (o *deleteOptions) run(beaker *beaker.Client) error {
+	ctx := context.TODO()
+
+	dataset, err := beaker.Dataset(ctx, o.dataset)
+	if err != nil {
+		return err
+	}
+
+	return dataset.Delete(ctx)
+}
