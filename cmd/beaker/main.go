6b78e12e6cb175769b4f0e3e066b7fecfde9fc5f
Lane Aasen <laneaasen@gmail.com>
2021-01-14 11:12:43 -0700

    Port experiment commands

--- cmd/beaker/main.go
+++ cmd/beaker/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/allenai/beaker/config"
+	"github.com/beaker/client/api"
 	"github.com/beaker/client/client"
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
 var beaker *client.Client
+var beakerConfig *config.Config
 var ctx context.Context
 var quiet bool
 var format string
@@ -27,13 +32,14 @@ func main() {
 
 	errorPrefix := color.RedString("Error:")
 
-	config, err := config.New()
+	var err error
+	beakerConfig, err = config.New()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s %+v\n", errorPrefix, err)
 		os.Exit(1)
 	}
 
-	beaker, err = client.NewClient(config.BeakerAddress, config.UserToken)
+	beaker, err = client.NewClient(beakerConfig.BeakerAddress, beakerConfig.UserToken)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s %+v\n", errorPrefix, err)
 		os.Exit(1)
@@ -52,6 +58,42 @@ func main() {
 
 	root.AddCommand(newClusterCommand())
 	root.AddCommand(newDatasetCommand())
+	root.AddCommand(newExperimentCommand())
 
 	root.Execute()
+}
+
+// ensureWorkspace ensures that workspaceRef exists or that the default workspace
+// exists if workspaceRef is empty.
+// Returns an error if workspaceRef and the default workspace are empty.
+func ensureWorkspace(workspaceRef string) (string, error) {
+	if workspaceRef == "" {
+		if beakerConfig.DefaultWorkspace == "" {
+			return "", errors.New(`workspace not provided, either:
+1. Pass the --workspace flag
+2. Configure a default workspace with 'beaker config set default_workspace <workspace>'`)
+		}
+		workspaceRef = beakerConfig.DefaultWorkspace
+	}
+
+	// Create the workspace if it doesn't exist.
+	if _, err := beaker.Workspace(ctx, workspaceRef); err != nil {
+		if apiErr, ok := err.(api.Error); ok && apiErr.Code == http.StatusNotFound {
+			parts := strings.Split(workspaceRef, "/")
+			if len(parts) != 2 {
+				return "", errors.New("workspace must be formatted like '<account>/<name>'")
+			}
+
+			if _, err = beaker.CreateWorkspace(ctx, api.WorkspaceSpec{
+				Organization: parts[0],
+				Name:         parts[1],
+			}); err != nil {
+				return "", err
+			}
+		} else {
+			return "", err
+		}
+	}
+
+	return workspaceRef, nil
 }
