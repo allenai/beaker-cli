e8153740c366a9a56945d9f9a13469118b78b91d
Lane Aasen <laneaasen@gmail.com>
2021-01-25 14:33:12 -0700

    Add dataset set-description command

--- cmd/beaker/dataset.go
+++ cmd/beaker/dataset.go
@@ -28,6 +28,7 @@ func newDatasetCommand() *cobra.Command {
 	cmd.AddCommand(newDatasetInspectCommand())
 	cmd.AddCommand(newDatasetLsCommand())
 	cmd.AddCommand(newDatasetRenameCommand())
+	cmd.AddCommand(newDatasetSetDescriptionCommand())
 	cmd.AddCommand(newDatasetStreamFileCommand())
 	return cmd
 }
@@ -321,6 +322,21 @@ func newDatasetRenameCommand() *cobra.Command {
 				fmt.Printf("Renamed %s to %s\n", color.BlueString(info.ID), info.DisplayID())
 			}
 			return nil
+		},
+	}
+}
+
+func newDatasetSetDescriptionCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "set-description <dataset> <description>",
+		Short: "Set the description of an dataset",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			dataset, err := beaker.Dataset(ctx, args[0])
+			if err != nil {
+				return err
+			}
+			return dataset.SetDescription(ctx, args[1])
 		},
 	}
 }
