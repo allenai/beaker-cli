447b562c3cacdd012f31ed8077c05804d4f79ef4
Lane Aasen <laneaasen@gmail.com>
2021-02-09 14:50:01 -0700

    Add executor command on mac but return an error

--- cmd/beaker/executor_darwin.go
+++ cmd/beaker/executor_darwin.go
@@ -1,8 +1,17 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 func newExecutorCommand() *cobra.Command {
-	// The executor is not supported on Mac.
-	return &cobra.Command{}
+	return &cobra.Command{
+		Use:   "executor <command>",
+		Short: "Manage the executor",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return fmt.Errorf("executor is not supported on Mac")
+		},
+	}
 }
