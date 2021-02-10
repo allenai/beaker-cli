f18c69133687a32aac8e8de74405f6fdf6c195ac
Lane Aasen <laneaasen@gmail.com>
2021-02-10 14:01:03 -0700

    Add executor stop command

--- cmd/beaker/executor_linux.go
+++ cmd/beaker/executor_linux.go
@@ -12,6 +12,7 @@ import (
 	"text/template"
 
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
 const (
@@ -27,11 +28,25 @@ const (
 	// Name of the executor's systemd service.
 	executorService = "beaker-executor"
 
-	// Default location for storing Beaker configuration.
-	defaultConfigDir = "/etc/beaker"
+	// Location for storing Beaker configuration.
+	executorConfigDir = "/etc/beaker"
 
 	// Default location for storing datasets.
 	defaultStorageDir = "/var/beaker"
+
+	// Path to the node file within the executor's storage directory.
+	executorNodePath = "node"
+)
+
+var (
+	// Path where executor configuration is stored.
+	executorConfigPath = path.Join(executorConfigDir, "config.yml")
+
+	// Path where the Beaker token used by the executor is stored.
+	executorTokenPath = path.Join(executorConfigDir, "token")
+
+	// Path where the Systemd configuration file for the executor is stored.
+	executorSystemdPath = fmt.Sprintf("/etc/systemd/system/%s.service", executorService)
 )
 
 var configTemplate = template.Must(template.New("config").Parse(`
@@ -85,13 +100,7 @@ Requires access to /etc, /var, and /usr/bin. Also requires access to systemd.`,
 		Args: cobra.ExactArgs(1),
 	}
 
-	var configDir string
 	var storageDir string
-	cmd.Flags().StringVar(
-		&configDir,
-		"config-dir",
-		defaultConfigDir,
-		"Writeable directory for Beaker configuration")
 	cmd.Flags().StringVar(
 		&storageDir,
 		"storage-dir",
@@ -99,42 +108,39 @@ Requires access to /etc, /var, and /usr/bin. Also requires access to systemd.`,
 		"Writeable directory for storing Beaker datasets")
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		if err := os.MkdirAll(configDir, os.ModePerm); err != nil {
+		if err := os.MkdirAll(executorConfigDir, os.ModePerm); err != nil {
 			return err
 		}
 
-		tokenPath := path.Join(configDir, "token")
 		if err := ioutil.WriteFile(
-			path.Join(configDir, "token"),
+			executorTokenPath,
 			[]byte(beakerConfig.UserToken),
 			0600,
 		); err != nil {
 			return err
 		}
 
-		configPath := path.Join(configDir, "config.yml")
-		configFile, err := os.Create(configPath)
+		configFile, err := os.Create(executorConfigPath)
 		if err != nil {
 			return err
 		}
 		defer configFile.Close()
 		if err := configTemplate.Execute(configFile, configOpts{
 			StoragePath: storageDir,
-			TokenPath:   tokenPath,
+			TokenPath:   executorTokenPath,
 			Cluster:     args[0],
 		}); err != nil {
 			return err
 		}
 
-		systemdPath := fmt.Sprintf("/etc/systemd/system/%s.service", executorService)
-		systemdFile, err := os.Create(systemdPath)
+		systemdFile, err := os.Create(executorSystemdPath)
 		if err != nil {
 			return err
 		}
 		defer systemdFile.Close()
 		if err := systemdTemplate.Execute(systemdFile, systemdOpts{
 			BinaryPath: executorPath,
-			ConfigPath: configPath,
+			ConfigPath: executorConfigPath,
 		}); err != nil {
 			return err
 		}
@@ -146,6 +152,30 @@ Requires access to /etc, /var, and /usr/bin. Also requires access to systemd.`,
 		return startExecutor()
 	}
 	return cmd
+}
+
+func newExecutorStopCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "stop",
+		Short: "Stop the executor and kill all running containers",
+		Args:  cobra.NoArgs,
+		func(cmd *cobra.Command, args []string) error {
+			if err := stopExecutor(); err != nil {
+				return err
+			}
+
+			node, err := getExecutorNode()
+			if err != nil {
+				return err
+			}
+			if err := beaker.Node(node).Delete(ctx); err != nil {
+				return err
+			}
+
+			// The executor cleanup command removes running containers.
+			return exec.CommandContext(ctx, executorPath, "cleanup")
+		}
+	}
 }
 
 func downloadExecutor() error {
@@ -198,4 +228,34 @@ func startExecutor() error {
 	}
 
 	return exec.CommandContext(ctx, "systemctl", "start", executorService).Run()
+}
+
+func stopExecutor() error {
+	if err := exec.CommandContext(ctx, "systemctl", "disable", executorService).Run(); err != nil {
+		return err
+	}
+
+	return exec.CommandContext(ctx, "systemctl", "stop", executorService).Run()
+}
+
+// Get the node ID of the executor running on this machine.
+func getExecutorNode() (string, error) {
+	configFile, err := os.Open(executorConfigPath)
+	if err != nil {
+		return "", err
+	}
+	defer configFile.Close()
+
+	var config struct {
+		StoragePath string `yaml:"storagePath"`
+	}
+	if err := yaml.NewDecoder(configFile).Decode(&config); err != nil {
+		return "", err
+	}
+
+	node, err := ioutil.ReadFile(path.Join(config.StoragePath, executorNodePath))
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(node)), nil
 }
